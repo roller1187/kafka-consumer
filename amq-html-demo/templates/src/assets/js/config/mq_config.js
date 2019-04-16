50605143adb9578fe918fb7606ace44889abe509
Andres Romero <aromerot@jupiter.fios-router.home>
2019-04-15 20:42:27 -0400

    Initial commit AMQ demo - roadshow 2

--- amq-html-demo/templates/src/assets/js/config/mq_config.js
+++
@@ -1,6 +0,0 @@
-var config = {
-    mq_username : "username",
-    mq_password : "password",
-    mq_server: "wss://localhost:443",
-    mq_destination: "myaddress"
-};
--- kafka-demo/src/main/java/com/redhat/kafkademo/KafkaDemoApplication.java
+++ kafka-demo/src/main/java/com/redhat/kafkademo/KafkaDemoApplication.java
@@ -1,17 +1,14 @@
 package com.redhat.kafkademo;
 
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class KafkaDemoApplication implements CommandLineRunner {
@@ -22,24 +19,14 @@ public class KafkaDemoApplication implements CommandLineRunner {
 		SpringApplication.run(KafkaDemoApplication.class, args);
 	}
 	
-	@Autowired
-    private KafkaTemplate<String, String> template;
-
     private final CountDownLatch latch = new CountDownLatch(10);
 
     @Override
-    public void run(String... args) throws Exception {
-    	for (int i = 0; i < 10; i++) {
-    		this.template.send("my-topic", "foo" + (i+1));
-    	}
-        latch.await(60, TimeUnit.SECONDS);
-        logger.info("All received");
-    }
+    public void run(String... args) throws Exception {}
 
     @KafkaListener(topics = "my-topic")
     public void listen(ConsumerRecord<?, ?> cr) throws Exception {
-        logger.info(cr.toString());
+        logger.info("RECEIVED ----> " + cr.toString());
         latch.countDown();
     }
-
 }
